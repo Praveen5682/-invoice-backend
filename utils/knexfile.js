3d2bd2e6736fe39fadaa8df34b77f9d42c58ac62
Praveen Kumar <praveensofficial1@gmail.com>
2026-03-26 08:22:46 +0530

    Updated

--- utils/knexfile.js
+++ utils/knexfile.js
@@ -1,38 +1,25 @@
 require("dotenv").config({
-  path: require("path").resolve(__dirname, "../.env"),
+  path: require("path").resolve(__dirname, "../.env"), // only used locally
 });
 
-// module.exports = {
-//   development: {
-//     client: "mysql2",
-//     connection: {
-//       host: process.env.DEV_DB_HOST,
-//       user: process.env.DEV_DB_USER,
-//       password: process.env.DEV_DB_PASSWORD,
-//       database: process.env.DEV_DB_NAME,
-//     },
-//     pool: { min: 2, max: 10 },
-//     debug: false,
-//   },
-
-//   production: {
-//     client: "mysql2",
-//     connection: {
-//       host: process.env.PROD_DB_HOST,
-//       user: process.env.PROD_DB_USER,
-//       password: process.env.PROD_DB_PASSWORD,
-//       database: process.env.PROD_DB_NAME,
-//     },
-//     pool: { min: 2, max: 10 },
-//     debug: false,
-//   },
-// };
-
-// console.log("🔥", process.env.DEV_DB_USER);
-
 module.exports = {
-  client: "mysql2",
-  connection: process.env.MYSQL_URL,
-  pool: { min: 2, max: 10 },
-  debug: false,
+  development: {
+    client: "mysql2",
+    connection: {
+      host: process.env.DEV_DB_HOST,
+      user: process.env.DEV_DB_USER,
+      password: process.env.DEV_DB_PASSWORD,
+      database: process.env.DEV_DB_NAME,
+      port: process.env.DEV_DB_PORT || 3306,
+    },
+    pool: { min: 2, max: 10 },
+    debug: false,
+  },
+
+  production: {
+    client: "mysql2",
+    connection: process.env.MYSQL_URL, // should be your Railway URL
+    pool: { min: 2, max: 10 },
+    debug: false,
+  },
 };
